02f4f8d35ed511137dcd239529df5327e9952442
Pushkar Kadam <pushkarkadam17@outlook.com>
2025-12-03 15:18:43 +1100

    added ways to read the columns as arrays

--- src/read_viapoints.cpp
+++ src/read_viapoints.cpp
@@ -3,16 +3,47 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <map>
 
-using namespace std;
+// using namespace std;
 
 int main() {
-    ifstream file("/home/robot1/Documents/robot_coords.csv");
+    std::ifstream file("/home/robot1/Documents/robot_coords.csv");
 
-    string line;
+    std::string line;
 
-    while (getline(file, line)) {
-        cout << line << endl;
+    std::getline(file, line);
+    std::stringstream headerStream(line);
+
+    std::vector<std::string> columnNames = {" ","x", "y", "z"};
+
+    // Map of column to list of values 
+    std::map<std::string, std::vector<double>> table;
+
+    for (const std::string& name : columnNames) {
+        table[name] = std::vector<double>();
+    }
+
+    // Read the rest of the rows 
+    while (std::getline(file, line)) {
+        std::stringstream row(line);
+        std::string cell;
+        int i = 0;
+
+        while (std::getline(row, cell, ',')) {
+            table[columnNames[i]].push_back(std::stod(cell));
+            i++;
+        }
+    }
+
+    // Print collected data
+    for (const std::string& name : columnNames) {
+        std::cout << name << ": ";
+
+        for (double value : table[name]) {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
     }
 
     return 0;
